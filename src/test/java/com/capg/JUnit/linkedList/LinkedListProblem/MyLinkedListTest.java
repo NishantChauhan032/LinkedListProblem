a17f74df5c5d011e4a084b8610c674cd251d0a26
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-15 01:54:11 +0530

    [NISHANT] add : Ability to remove an element from the list

--- src/test/java/com/capg/JUnit/linkedList/LinkedListProblem/MyLinkedListTest.java
+++ src/test/java/com/capg/JUnit/linkedList/LinkedListProblem/MyLinkedListTest.java
@@ -137,4 +137,26 @@ public class MyLinkedListTest {
 		Assert.assertTrue(result);
 	}
 
+	@Test
+	public void givenANumber_whenRemoved_shouldBeDeletedFromList() {
+		MyNode<Integer> myFirstNode = new MyNode(56);
+		MyNode<Integer> mySecondNode = new MyNode(30);
+		MyNode<Integer> myThirdNode = new MyNode(40);
+		MyNode<Integer> myForthNode = new MyNode(70);
+		
+		MyLinkedList myLinkedList = new MyLinkedList();
+		
+		myLinkedList.append(myFirstNode);
+		myLinkedList.append(mySecondNode);
+		myLinkedList.append(myThirdNode);
+		myLinkedList.append(myForthNode);
+		myLinkedList.delete(40);
+		
+		myLinkedList.printMyNodes();
+		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
+				&& myLinkedList.tail.equals(myForthNode);
+		Assert.assertTrue(result);
+		
+	}
+
 }
